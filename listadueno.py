589db3d2be49257cbb2f3743b4f66657952588a0
unknown <darioguille92@gmail.com>
2022-08-11 23:42:55 -0300

    Lista dueno en Phyton

---
+++ listadueno.py
@@ -0,0 +1,14 @@
+#1) Crear una lista en Python denominada “Dueno” que contenga los siguientes valores:
+#  28957346,  Juan,  Perez, 4789689,  Belgrano 101                           
+#Dichos datos se corresponden  con los datos del dueño de un perro
+#(DNI, nombre, apellido, teléfono y dirección).
+#Muestre en pantalla el teléfono del dueño si el DNI es mayor a 26000000.
+
+Dueno = [28957346,"juan","peres",4789689,"Belgrano 101"]
+
+dni, nombre, apellido, telefono, direccion = Dueno
+
+if dni > 26000000:
+    print("telefono: ",telefono)
+else:
+    print("No Disponible")

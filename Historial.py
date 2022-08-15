0cf3c7691fdc1229f00579a88180696cdc6368cc
Perez Fernando <95585993+pfdevv@users.noreply.github.com>
2022-08-14 23:24:06 -0300

    Add files via upload

---
+++ Historial.py
@@ -0,0 +1,8 @@
+Historial = (2350, 5960, 23000, 1000, 8900)
+
+gastos_Puppy = sum(Historial)
+
+if gastos_Puppy < 30000:
+ print(gastos_Puppy)
+else:
+ print("Gastos por encima de lo presupuestado")

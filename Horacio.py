daecc0c1bf922093a22889e377a85b0f21be246b
HoracioeQuiroga <horacioequiroga2020@gmail.com>
2022-08-15 00:04:11 -0300

    se resuelven ejercicios 2 y 11

---
+++ Horacio.py
@@ -0,0 +1,11 @@
+Dueno2 =[23546987,"Maria","perez",4789689,"Pueyrredon 811"]
+
+DNI, nombre, apellido, teléfono, dirección = Dueno2
+
+for i in Dueno2:
+    if i == DNI:
+        continue
+    if i == apellido :
+        continue
+    
+    print(i)
